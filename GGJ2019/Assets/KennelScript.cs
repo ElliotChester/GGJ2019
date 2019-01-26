f8b5d3397b6114c0fc39c24f96ea00140a8f69b2
Elliot Chester <elliotchester@outlook.com>
2019-01-26 20:27:57 +0000

    kennel now falls when in mid air

---
+++ GGJ2019/Assets/KennelScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KennelScript : MonoBehaviour
+{
+
+    public float fallSpeed;
+
+    void Update()
+    {
+        if (!IsGrounded())
+        {
+            this.transform.position -= Vector3.up * Time.deltaTime * fallSpeed;
+        }
+    }
+
+    bool IsGrounded()
+    {
+        Debug.DrawRay(this.transform.position, Vector3.down, Color.red, 5);
+        RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, Vector2.down, out hit, 0.75f))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
