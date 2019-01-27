143679950ff856155185165c0d8a0f787d4be433
Sean Turner <s188119@uos.ac.uk>
2019-01-27 01:57:26 +0000

    music and rain and things

---
+++ GGJ2019/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public AudioClip music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		this.GetComponent<AudioSource>().PlayOneShot(music);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
