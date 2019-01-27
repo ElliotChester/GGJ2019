d47c07b795d60f6491a0e645fb9ea6a5ab08cbe4
Sean Turner <s188119@uos.ac.uk>
2019-01-27 04:21:13 +0000

    lots of level things

--- GGJ2019/Assets/Scripts/GameManager.cs
+++ GGJ2019/Assets/Scripts/GameManager.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 	public AudioClip music;
+	public AudioClip rain;
 
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
     {
-		this.GetComponent<AudioSource>().PlayOneShot(music);
+		this.GetComponent<AudioSource>().Play();
     }
 
     // Update is called once per frame
